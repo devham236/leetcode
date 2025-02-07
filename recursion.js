d680e0b72bc0977ab681b845edf0c027f72d49fc
Hamza Mirza <hamza665@hotmail.de>
2025-02-07 11:44:41 +0100

    added more recursion examples

--- recursion.js
+++ recursion.js
@@ -1,3 +1,4 @@
+/* Example 1 */
 function fn(i) {
   if (i > 3) return;
 
@@ -8,5 +9,72 @@ function fn(i) {
 }
 
 fn(1);
+/* Output */
+// 1
+// 2
+// 3
+// End of call where i = 3
+// End of call where i = 2
+// End of call where i = 1
 
-// Every function call "exists" until it returns. When we move to a different function call, the old one waits until the new one returns. The order in which the calls happens is remembered, and the lines within the functions are executed in order.
+/* Example 2 */
+function countDown(n) {
+  if (n <= 0) return;
+
+  console.log(n);
+  countDown(n - 1);
+  console.log(`Returning from n = ${n}`);
+}
+
+countDown(3);
+/* Output */
+// 3
+// 2
+// 1
+// Returning from n = 1
+// Returning from n = 2
+// Returning from n = 3
+
+/* Example 3 */
+function doubleCall(n) {
+  if (n > 2) return;
+
+  console.log(`Start call n = ${n}`);
+  doubleCall(n + 1);
+  doubleCall(n + 1);
+  console.log(`End call n = ${n}`);
+}
+
+doubleCall(0);
+
+/* Output */
+// Start call n = 0
+// Start call n = 1
+// Start call n = 2
+// End call n = 2
+// Start call n = 2
+// End call n = 2
+// End call n = 1
+// Start call n = 1
+// Start call n = 2
+// End call n = 2
+// Start call n = 2
+// End call n = 2
+// End call n = 1
+// End call n = 0
+
+/* Example 4 */
+function sum(n) {
+  if (n === 1) return 1;
+
+  let result = n + sum(n - 1);
+  console.log(`Sum at n = ${n} is ${result}`);
+  return result;
+}
+
+sum(4);
+
+/* Output */
+// Sum at n = 2 is 3
+// Sum at n = 3 is 6
+// Sum at n = 4 is 10
